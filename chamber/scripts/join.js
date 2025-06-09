0da9b9c1b9e2139256a2a10d57ef5a908565358b
malikantsi <lekhoanalucy@gmail.com>
2025-06-09 11:33:41 +0200

    animation fixed

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -20,24 +20,37 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('show');
+        }, index * 300); // stagger by 300ms per card
+        console.log(card);
+    });
+});
 
 const getstring = window.location.search;
 const myInfo = new URLSearchParams(getstring);
 console.log(myInfo.get("form-timestamp"));
 
 const timestamp = new Date().toISOString();
-// document.getElementById("form-timestamp").value = timestamp;
-document.getElementById("timestamp-display").textContent = timestamp;
+console.log(timestamp);
 
-document.querySelector("#results").innerHTML = `
-<p>Application is for ${myInfo.get('first')} ${myInfo.get('last')}</p>
-<p>Organizational title is ${myInfo.get('Organizationaltitle')} </p>
-<p>Email :${myInfo.get('email')} </p>
-<p>Phone :${myInfo.get('phone')} </p>
-<p>Organization's Name is ${myInfo.get('business-name')} </p>
-<p>Membership Level:${myInfo.get('membership-level')} </p>
-<p>Organization Description:${myInfo.get('org-description')} </p>
-<p>Time of registration: ${myInfo.get("form-timestamp")}</p> 
+// document.getElementById("timestamp-display").value = timestamp;
 
-
-`;
+console.log(myInfo.has('first'));
+if (myInfo.has('first') == true) {
+    document.querySelector("#results").innerHTML = `
+    <p>Application is for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+    <p>Organizational title is ${myInfo.get('Organizationaltitle')} </p>
+    <p>Email :${myInfo.get('email')} </p>
+    <p>Phone :${myInfo.get('phone')} </p>
+    <p>Organization's Name is ${myInfo.get('business-name')} </p>
+    <p>Membership Level:${myInfo.get('membership-level')} </p>
+    <p>Organization Description:${myInfo.get('org-description')} </p>
+    <p>Time of registration: ${myInfo.get("form-timestamp")}</p> `;
+}
+else {
+    document.getElementById("form-timestamp").value = timestamp;
+}
