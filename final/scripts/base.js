8eebf8f17be0958a27e72f7cb25bc21dd0a90c68
malikantsi <lekhoanalucy@gmail.com>
2025-06-19 12:55:15 +0200

    some updates

--- final/scripts/base.js
+++ final/scripts/base.js
@@ -1,3 +1,23 @@
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+// Close modal when clicking outside of it
+window.onclick = function (event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
+
+
+
 function initMap() {
     const location = { lat: -26.2670574, lng: 27.7714954 };
     const map = new google.maps.Map(document.getElementById("map"), {
